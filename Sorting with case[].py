b37f7860a74afd3b82d9aa61ba39ff417a544542
Malenkey <keithcalica@gmail.com>
2023-01-25 20:14:17 +0000

    xcvx

--- Sorting with case[].py
+++ Sorting with case[].py
@@ -1,1 +1,9 @@
-print('whats up fuckers')
+comparison_list = ["one", "two", "three"]
+
+match comparison_list:
+    case [first]:
+        print("this is the first element: {first}")
+    case [first, *rest]:
+        print("This is the first: {first}, and this is the rest: {rest}")
+    case _:
+        print("Nothing was matched")
